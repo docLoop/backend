7991b117496407c9dbabf8bb336ea0315ef1e375
rhotep <andreas.pittrich@posteo.de>
2017-10-24 05:15:03 +0200

    dwd

--- js/docloop-core.js
+++ js/docloop-core.js
@@ -47,13 +47,13 @@ class docLoopCore extends EventEmitter {
 								}
 		}))
 
-		this.app.use(function(req, res, next) {
-			res.header('Access-Control-Allow-Credentials', 	true)
-			res.header('Access-Control-Allow-Origin', 		req.headers.origin)
-			res.header('Access-Control-Allow-Methods', 		'GET,PUT,POST,DELETE')
-			res.header('Access-Control-Allow-Headers', 		'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept')
-			next();
-		});
+		// this.app.use(function(req, res, next) {
+		// 	res.header('Access-Control-Allow-Credentials', 	true)
+		// 	res.header('Access-Control-Allow-Origin', 		req.headers.origin)
+		// 	res.header('Access-Control-Allow-Methods', 		'GET,PUT,POST,DELETE')
+		// 	res.header('Access-Control-Allow-Headers', 		'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept')
+		// 	next()		
+		// })
 
 
 		this.app.use(bodyParser.json())
